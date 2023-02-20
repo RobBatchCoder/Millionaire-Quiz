e437ae167bcb82c444873d552963a853d4813b53
RobBatchCoder <robertbatchelor909@gmail.com>
2023-02-20 16:20:35 +0000

    add check answers function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,17 @@
 var questions = [{
-    question: 'What is the capital of FrENCHIESA?',
+    question: 'What is the capital of France?',
     answerA: 'Moscow',
     answerB: 'Paris',
     answerC: 'Leon',
     answerD: 'Madrid',
-    result: 'answerB'
+    result: 'answer-B'
 }, {
     question: 'What are the three primary colours?',
     answerA: 'Red, Yellow, Blue',
     answerB: 'Red, Green, Yellow',
     answerC: 'White, Black, Gold',
     answerD: 'Red, Green Blue',
-    result: 'answerD'
+    result: 'answer-D'
 }];
 
 function setup() {
@@ -31,11 +31,20 @@ function setup() {
 }
 
 function checkAnswer(type) {
-    alert(type);
+    if (type === questions[0].result) {
+        alert('Yeah, you got it right!');
+    }else{
+        alert('You suck!');
+    }
+
 }
 
 function main() {
     document.getElementById('question-box').innerText = questions[0].question;
+    document.getElementById('answer-A').innerText = questions[0].answerA;
+    document.getElementById('answer-B').innerText = questions[0].answerB;
+    document.getElementById('answer-C').innerText = questions[0].answerC;
+    document.getElementById('answer-D').innerText = questions[0].answerD;
 }
 
 // Main program

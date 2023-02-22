cefc1d6157b7014c6c14798830e8b66d8973d39a
RobBatchCoder <robertbatchelor909@gmail.com>
2023-02-22 11:19:13 +0000

    simplied questions object structure

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,112 +13,113 @@ var questions = [{
         C: 'White, Black, Gold',
         D: 'Red, Green, Blue',
         result: 'D'
+    },
+
+    {
+        question: 'In which Italian city can you find the Colosseum?',
+        A: 'Venice',
+        B: 'Rome',
+        C: 'Naples',
+        D: 'Milan',
+        result: 'B'
+    },
+    {
+        question: 'What is the largest active volcano in the world?',
+        A: 'Mount Etna',
+        B: 'Mount Vesuvius',
+        C: 'Mouna Loa',
+        D: 'Mount Batur',
+        result: 'C'
+    },
+    {
+        question: 'When did Lenin die?',
+        A: '21 January 1924',
+        B: '21 January 1934',
+        C: '21 January 1944',
+        D: '21 January 1954',
+        result: 'A'
+    },
+    {
+        question: 'When was the book “To Kill a Mockingbird” by Harper Lee published?',
+        A: '1950',
+        B: '1960',
+        C: '1970',
+        D: '1980',
+        result: 'B'
+    },
+    {
+        question: "In which museum can you find Leonardo Da Vinci’s Mona Lisa?",
+        A: 'Le Louvre',
+        B: 'Uffizi Museum',
+        C: 'British Museum',
+        D: 'Metropolitan Museum of Art',
+        result: 'A'
+    },
+    {
+        question: 'When did Hitler invade Poland?',
+        A: '1st September 1939',
+        B: '11th November 1939',
+        C: '8th May 1940',
+        D: '1st December 1940',
+        result: 'A'
+    },
+    {
+        question: 'What is the largest continent in size?',
+        A: 'Asia',
+        B: 'Africa',
+        C: 'Europe',
+        D: 'North America',
+        result: 'A'
+    },
+    {
+        question: 'Which French king was nicknamed the “Sun King”?',
+        A: 'Louis XVI',
+        B: 'Charlemagne',
+        C: 'Francis I',
+        D: 'Louis XIV',
+        result: 'D'
+    },
+    {
+        question: 'Who was George W. Bush’ vice president?',
+        A: 'Kamala Harris',
+        B: 'Joe Biden',
+        C: 'Dick Cheney',
+        D: 'Al Gore',
+        result: 'C'
+    },
+    {
+        question: 'Which famous inventor invented the telephone?',
+        A: 'Thomas Edison',
+        B: 'Benjamin Franklin',
+        C: 'Alexander Graham Bell',
+        D: 'Nikola Tesla',
+        result: 'C'
+    },
+    {
+        question: 'Which famous inventor invented the telephone?',
+        A: 'Thomas Edison',
+        B: 'Benjamin Franklin',
+        C: 'Alexander Graham Bell',
+        D: 'Nikola Tesla',
+        result: 'C'
+    },
+    {
+        question: 'In which city is the Juventus Football Club based?',
+        A: 'Turin',
+        B: 'Barcelona',
+        C: 'Manchester',
+        D: 'Marseille',
+        result: 'A'
+    },
+    {
+        question: 'What does the Richter scale measure?',
+        A: 'Wind Speed',
+        B: 'Temperature',
+        C: 'Tornado Strength',
+        D: 'Earthquake intensity',
+        result: 'D'
     }
 ];
-// {
-//     question: 'In which Italian city can you find the Colosseum?',
-//     answerA: 'Venice',
-//     answerB: 'Rome',
-//     answerC: 'Naples',
-//     answerD: 'Milan',
-//     result: 'answer-B'
-// },
-// {
-//     question: 'What is the largest active volcano in the world?',
-//     answerA: 'Mount Etna',
-//     answerB: 'Mount Vesuvius',
-//     answerC: 'Mouna Loa',
-//     answerD: 'Mount Batur',
-//     result: 'answer-C'
-// },
-// {
-//     question: 'When did Lenin die?',
-//     answerA: '21 January 1924',
-//     answerB: '21 January 1934',
-//     answerC: '21 January 1944',
-//     answerD: '21 January 1954',
-//     result: 'answer-A'
-// },
-// {
-//     question: 'When was the book “To Kill a Mockingbird” by Harper Lee published?',
-//     answerA: '1950',
-//     answerB: '1960',
-//     answerC: '1970',
-//     answerD: '1980',
-//     result: 'answer-B'
-// },
-// {
-//     question: "In which museum can you find Leonardo Da Vinci’s Mona Lisa?",
-//     answerA: 'Le Louvre',
-//     answerB: 'Uffizi Museum',
-//     answerC: 'British Museum',
-//     answerD: 'Metropolitan Museum of Art',
-//     result: 'answer-A'
-// },
-// {
-//     question: 'When did Hitler invade Poland?',
-//     answerA: '1st September 1939',
-//     answerB: '11th November 1939',
-//     answerC: '8th May 1940',
-//     answerD: '1st December 1940',
-//     result: 'answer-A'
-// },
-// {
-//     question: 'What is the largest continent in size?',
-//     answerA: 'Asia',
-//     answerB: 'Africa',
-//     answerC: 'Europe',
-//     answerD: 'North America',
-//     result: 'answer-A'
-// },
-// {
-//     question: 'Which French king was nicknamed the “Sun King”?',
-//     answerA: 'Louis XVI',
-//     answerB: 'Charlemagne',
-//     answerC: 'Francis I',
-//     answerD: 'Louis XIV',
-//     result: 'answer-D'
-// },
-// {
-//     question: 'Who was George W. Bush’ vice president?',
-//     answerA: 'Kamala Harris',
-//     answerB: 'Joe Biden',
-//     answerC: 'Dick Cheney',
-//     answerD: 'Al Gore',
-//     result: 'answer-C'
-// },
-// {
-//     question: 'Which famous inventor invented the telephone?',
-//     answerA: 'Thomas Edison',
-//     answerB: 'Benjamin Franklin',
-//     answerC: 'Alexander Graham Bell',
-//     answerD: 'Nikola Tesla',
-//     result: 'answer-C'
-// },
-// {
-//     question: 'Which famous inventor invented the telephone?',
-//     answerA: 'Thomas Edison',
-//     answerB: 'Benjamin Franklin',
-//     answerC: 'Alexander Graham Bell',
-//     answerD: 'Nikola Tesla',
-//     result: 'answer-C'
-// },
-// {
-//     question: 'In which city is the Juventus Football Club based?',
-//     answerA: 'Turin',
-//     answerB: 'Barcelona',
-//     answerC: 'Manchester',
-//     answerD: 'Marseille',
-//     result: 'answer-A'
-// },
-// {
-//     question: 'What does the Richter scale measure?',
-//     answerA: 'Wind Speed',
-//     answerB: 'Temperature',
-//     answerC: 'Tornado Strength',
-//     answerD: 'Earthquake intensity',
-//     result: 'answer-D'
-// },
 
 
 var score = 0;
